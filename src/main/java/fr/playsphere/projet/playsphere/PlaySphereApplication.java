df58e6534a853f028e8dba777b99f7d4652e41d4
Emilie Castellano-web <emiliecastellano@hotmail.com>
2025-02-27 19:09:52 +0100

    Premier commit

---
+++ src/main/java/fr/playsphere/projet/playsphere/PlaySphereApplication.java
@@ -0,0 +1,13 @@
+package fr.playsphere.projet.playsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaySphereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaySphereApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/playsphere/projet/playsphere/PlaySphereApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.playsphere.projet.playsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaySphereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
